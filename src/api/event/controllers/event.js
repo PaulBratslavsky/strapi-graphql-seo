6bf4540501aebed23eb62047e4c26dbc47f005f7
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-08-19 10:37:30 -0500

    added event get by slug route and controller

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -3,7 +3,20 @@
 /**
  *  event controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ( {strapi}) => ({
+  async findBySlug(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::event.event", query);
+    const sanitizedEntity = await this.sanitizeOutput(post);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
