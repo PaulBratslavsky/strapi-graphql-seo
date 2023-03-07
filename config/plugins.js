0b460b11c353fbf7517c50b0254c3ebc50b187c7
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-03-07 01:14:40 -0600

    configured email

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,20 @@ module.exports = ({ env }) => ({
     seo: {
       enabled: true,
     },  
+
+    email: {
+      config: {
+        provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+        providerOptions: {
+          apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+          defaultFrom: 'paul@codingafterthirty.com',
+          defaultReplyTo: 'paul@codingafterthirty.com',
+          testAddress: 'paul@codingafterthirty.com',
+        },
+      },
+    },
     
     upload: {
         config: {
